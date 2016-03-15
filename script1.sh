0952767ba3ec6875ab8d86f4b6d97a9b986e4f7c
Andrii Kroshchenko debian_test <akroshchenko@mirantis.com>
2016-03-15 16:02:44 +0200

    task 1: finished but unchecked

--- script1.sh
+++ script1.sh
@@ -1,13 +1,8 @@
 #!/bin/bash
 
-usage="example of usage  script.sh (add-rep|pick|) (stable|testing|unstable) URI (main|contrib|non-free)"
 
-
-if [ ! -f /etc/apt/sources.list.back ]
-then    
-        echo "creating /etc/apt/sources.list.bak"
-        echo "cp /etc/apt/sources.list /etc/apt/sources.list.back"
-fi
+add_rep_usage="script.sh add-rep (stable|testing|unstable) URI "
+pin_usage="script.sh pin URI 'name_package'(mya content globbing)"
 
 
 no_rep_exist () {
@@ -19,7 +14,7 @@ then
 	then
 		echo "repository alreade exist"
 	else
-		echo "goog repo"
+		echo "repository $uri added"
 		return 0
 	fi
 fi
@@ -30,29 +25,50 @@ return 1
 #body of the script
 
 case $1 in
-	add-rep) case $2 in
+	add-rep)
+		if [ ! -f /etc/apt/sources.list.back ]
+		then
+			echo "creating /etc/apt/sources.list.back"
+ 	                cp /etc/apt/sources.list /etc/apt/sources.list.back
+		fi
+		 case $2 in
 			  stable) 
-				echo "s2 stable"
 				if (no_rep_exist $3 $2)
 				then
-					echo "deb $3 $2 main contrib non-free"
+					echo "### new $2 repository ###" >> /etc/apt/sources.list 
+					echo "deb $3 $2 main contrib non-free" >> /etc/apt/sources.list
 				fi 
 			  	;;
-			 testing) echo "s2 testing" 
+			 testing) 
 				if (no_rep_exist $3 $2)
 				then
-					echo "deb $3 $2 main contrib non-free"
+					echo "### new $2 repository ###" >> /etc/apt/sources.list
+                                        echo "deb $3 $2 main contrib non-free" >> /etc/apt/sources.list
 				fi	
 				;;
-			unstable) echo "s2 unstable" 
+			unstable)
 				if (no_rep_exist $3 $2)
 				then
-					 echo "deb $3 $2 main contrib non-free"
+					 echo "### new $2 repository ###" >> /etc/apt/sources.list
+                                         echo "deb $3 $2 main contrib non-free" >> /etc/apt/sources.list
 				fi
 				;;
-			       *) echo "need choose  (stable|testing|unstable)"
+			       *) echo $add_rep_usage
 		 esac
 		;;
-	   pick) echo "pick " ;;
-	      *) echo $usage
+	   pin)
+		if [ ! -f /etc/apt/preferences ]
+		then 
+			touch /etc/apt/preferences
+		else
+			echo "creating /etc/apt/preferences.back"
+			cp /etc/apt/preferences /etc/apt/preferences.back
+		fi
+		
+		printf "Package: %s \nPin origin \"%s\"\nPin-Priority: 1001\n\n" $3 "$2" >> /etc/apt/preferences
+		 ;;
+	      *) echo "varient of usage"
+		 echo $add_rep_usage
+		 echo $pin_usage
+
 esac
